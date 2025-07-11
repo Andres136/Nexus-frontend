30118486c37ce1e6485e2f02f320efb0b600a4a4
Elver581 <elveral100@gmail.com>
2025-07-11 16:00:18 -0500

    Agregar scroll documentacion

--- src/components/calidad/ProcesosDepartamento.jsx
+++ src/components/calidad/ProcesosDepartamento.jsx
@@ -168,7 +168,7 @@ const confirmarEliminacion = (documentoId) => {
     </div>
 
         {/* Columna 2: Documentación Disponible */}
-        <div className="bg-white p-6 rounded-lg shadow-md h-64 overflow-y-auto">
+        <div className="bg-white p-6 rounded-lg shadow-md overflow-y-auto">
       <h3 className="text-xl font-semibold mb-4 text-gray-800">
         {procesoActual?.nombre || "No has seleccionado una carpeta"}
       </h3>
